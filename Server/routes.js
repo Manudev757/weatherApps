4cb3fc78da0d79d69f3ef9488f9d06310d6eaec5
Manudev <manudev.rethina@solitontech.com>
2023-02-20 12:09:55 +0530

    Refactor : Added comments to the working code

--- Server/routes.js
+++ Server/routes.js
@@ -1,15 +1,24 @@
 const path = require("path");
 const fs = require("fs");
+
+//Importing the Required functions from timzone.js
 const {
   allTimeZones,
   timeForOneCity,
   nextNhoursWeather,
 } = require("./timeZone");
+
+//storing the city data in a variable
 const cityData = allTimeZones();
+
+//function call from the request
 const weatherForecast = (req, res) => {
+  //specifying a default path '/' when page loaded which serves index.html file
   let filePath = path.join(req.url === "/" ? "Index.html" : req.url.slice(1));
   let extName = path.extname(filePath);
   let contentType = "text/html";
+
+  //identifying the extention of files requested to server
   switch (extName) {
     case ".css":
       contentType = "text/css";
@@ -21,20 +30,27 @@ const weatherForecast = (req, res) => {
       contentType = "image/svg+xml";
       break;
   }
+
+  // Ignoring files with extension .ico and 'Nil'
   if (extName == ".ico" || filePath == "Nil") {
     res.end();
     return;
   }
+  // gets all the city data
   if (req.url === "/allTimeZone") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.write(JSON.stringify(allTimeZones()));
     res.end();
-  } else if (req.url.startsWith("/city")) {
+  }
+  //gets a detail of given city
+  else if (req.url.startsWith("/city")) {
     const city = req.url.split("/")[2];
     res.writeHead(200, { "Content-Type": "application/json" });
     res.write(JSON.stringify(timeForOneCity(city)));
     res.end();
-  } else if (req.url == "/hourlyForecast") {
+  }
+  //getting next 5 hours data by providing a city details of a particular city
+  else if (req.url == "/hourlyForecast") {
     const body = [];
     req.on("data", (chunk) => {
       body.push(chunk);
@@ -50,7 +66,9 @@ const weatherForecast = (req, res) => {
       );
       res.end();
     });
-  } else {
+  }
+  //Sending the html file to the browser
+  else {
     res.writeHead(200, { "Content-Type": contentType });
     fs.createReadStream(filePath).pipe(res);
   }
